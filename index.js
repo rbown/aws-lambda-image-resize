98736fd04f07d01e28375ea36ea43087610b7082
Ryan Bown <rbown@niftee.com.au>
2017-04-03 23:18:29 +0800

    Functional Updates

--- index.js
+++ index.js
@@ -1,24 +1,15 @@
 // dependencies
 var async = require('async');
 var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
-var gm = require('gm')
-    .subClass({
-        imageMagick: true
-    }); // Enable ImageMagick integration. Built into Lambda
+var gm = require('gm').subClass({ imageMagick: true });
+var path = require('path');
 var util = require('util');
+var s3 = new AWS.S3();
 
-var THUMB_KEY_PREFIX = "players/thumbs/";
-
-// constants
-var sizeConfigs = [{
-    postfix: '_w200',
-    width: 200
-}, 
-/*{
-    postfix: '_w100',
-    width: 100
-}*/];
+var THUMB_KEY_PREFIX = "players/thumbs/",
+    MAX_WIDTH = 200,
+    MAX_HEIGHT = 300,
+    ALLOWED_FILETYPES = ['jpg', 'jpeg']; // Other extensions can be added ['png', 'jpg', 'jpeg', 'bmp', 'tiff', 'pdf', 'gif']
 
 var utils = {
   decodeKey: function(key) {
@@ -27,93 +18,79 @@ var utils = {
 };
 
 exports.handler = function(event, context) {
+    var srcBucket = event.Records[0].s3.bucket.name,
+    srcKey = utils.decodeKey(event.Records[0].s3.object.key), // eg. /documents/new-document.pdf
+    srcPath = path.parse(srcKey),
+    fileType = srcKey.match(/\.\w+$/),
+    filename = srcPath.name,
+    dstKey = THUMB_KEY_PREFIX + filename + fileType;
 
-    var bucket = event.Records[0].s3.bucket.name,
-    srcKey = utils.decodeKey(event.Records[0].s3.object.key),
-    dstKey = THUMB_KEY_PREFIX + srcKey.replace(/\.\w+$/, ".jpg"),
-    fileType = srcKey.match(/\.\w+$/);
-
-    var typeMatch = srcKey.match(/\.([^.]*)$/);
-    if (!typeMatch) {
-        console.error('unable to infer image type for key ' + srcKey);
-        return context.fail();
+    if (fileType === null) {
+        console.error("Invalid filetype found for key: " + srcKey);
+        return;
     }
-    
-    var imageType = typeMatch[1].toLowerCase();
-    if (imageType != "jpg" && imageType != "jpeg" && imageType != "png") {
-        console.log('skipping non-image ' + srcKey);
-        return context.fail();
+
+    fileType = fileType[0].substr(1);
+
+    if (ALLOWED_FILETYPES.indexOf(fileType) === -1) {
+        console.error("Filetype " + fileType + " not valid for thumbnail, exiting...");
+        return;
     }
 
     // Download the image from S3, transform, and upload to a different S3 bucket.
+     // Download the image from S3, transform, and upload to a different S3 bucket.
     async.waterfall([
         function download(next) {
+            // Download the image from S3 into a buffer.
             s3.getObject({
-                    Bucket: bucket,
+                    Bucket: srcBucket,
                     Key: srcKey
                 },
                 next);
-        },
-        function tranform(response, next) {
-            async.map(sizeConfigs, resize, function(err, mapped) {
-                next(err, mapped);
-            });
+            },
+        function transform(response, next) {
+            gm(response.Body).size(function(err, size) {
+                // Infer the scaling factor to avoid stretching the image unnaturally.
+                var scalingFactor = Math.min(
+                    MAX_WIDTH / size.width,
+                    MAX_HEIGHT / size.height
+                );
+                var width  = scalingFactor * size.width;
+                var height = scalingFactor * size.height;
 
-            function resize(config, callback) {
-                gm(response.Body)
-                    .size(function(err, size) {
-                        if(err){next(err);}
-                        
-                        var width = config.width;
-                        var height = null;
-
-                        this.resize(width, height)
-                            .toBuffer('jpg', function(err, buffer) {
-                                //  console.log('toBuffer');
-                                if (err) {
-                                    console.error(err);
-                                    callback(err);
-                                }
-                                else {
-                                    var obj = config;
-                                    //obj.contentType = 'image/jpeg';
-                                    obj.data = buffer;
-                                    callback(null, obj);
-                                }
-                            });
+                // Transform the image buffer in memory.
+                this.resize(width, height)
+                    .toBuffer(fileType, function(err, buffer) {
+                        if (err) {
+                            next(err);
+                        } else {
+                            next(null, response.ContentType, buffer);
+                        }
                     });
-            }
+            });
         },
-        function upload(items, next) {
-
-            async.each(items,
-                function(item, callback) {
-                    s3.putObject({
-                        Bucket: bucket,
-                        Key: dstKey,
-                        Body: item.data,
-                        ContentType: 'image/jpeg'
-                    }, callback);
+        function upload(contentType, data, next) {
+            // Stream the transformed image to a different S3 bucket.
+            s3.putObject({
+                    Bucket: srcBucket,
+                    Key: dstKey,
+                    Body: data,
+                    ContentType: contentType
                 },
-                function(err) {
-                    next(err);
-                });
+                next);
+            }
+        ], function (err) {
+            if (err) {
+                console.error(
+                    'Unable to resize ' + srcBucket + '/' + srcKey + ' and upload to ' + srcBucket + '/' + dstKey + ' due to an error: ' + err
+                );
+            } else {
+                console.log(
+                    'Successfully resized ' + srcBucket + '/' + srcKey + ' and uploaded to ' + srcBucket + '/' + dstKey
+                );
+            }
 
-        }
-    ], function(err) {
-        if (err) {
-            console.error(
-                'Unable to resize ' + bucket + '/' + srcKey +
-                ' and upload to ' + bucket + '/' + dstKey +
-                ' due to an error: ' + err
-            );
-        }
-        else {
-            console.log(
-                'Successfully resized ' + bucket + '/' + dstKey +
-                ' and uploaded to ' + bucket + '/' + dstKey
-            );
             context.done();
         }
-    });
+    );
 };
