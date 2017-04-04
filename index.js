0ed34ed6e73fe027d1b4f11ed7e0455b98ed953b
ryanbown <ryan@roobix.com.au>
2017-04-04 09:57:20 +0800

    Fix for sub folder

--- index.js
+++ index.js
@@ -25,72 +25,77 @@ exports.handler = function(event, context) {
     filename = srcPath.name,
     dstKey = THUMB_KEY_PREFIX + filename + fileType;
 
-    if (fileType === null) {
-        console.error("Invalid filetype found for key: " + srcKey);
-        return;
-    }
+    if (srcKey.indexOf('/thumbs') === -1) {
+        // Not in thumbs folder
 
-    fileType = fileType[0].substr(1);
-
-    if (ALLOWED_FILETYPES.indexOf(fileType) === -1) {
-        console.error("Filetype " + fileType + " not valid for thumbnail, exiting...");
-        return;
-    }
-
-    // Download the image from S3, transform, and upload to a different S3 bucket.
-     // Download the image from S3, transform, and upload to a different S3 bucket.
-    async.waterfall([
-        function download(next) {
-            // Download the image from S3 into a buffer.
-            s3.getObject({
-                    Bucket: srcBucket,
-                    Key: srcKey
-                },
-                next);
-            },
-        function transform(response, next) {
-            gm(response.Body).size(function(err, size) {
-                // Infer the scaling factor to avoid stretching the image unnaturally.
-                var scalingFactor = Math.min(
-                    MAX_WIDTH / size.width,
-                    MAX_HEIGHT / size.height
-                );
-                var width  = scalingFactor * size.width;
-                var height = scalingFactor * size.height;
-
-                // Transform the image buffer in memory.
-                this.resize(width, height)
-                    .toBuffer(fileType, function(err, buffer) {
-                        if (err) {
-                            next(err);
-                        } else {
-                            next(null, response.ContentType, buffer);
-                        }
-                    });
-            });
-        },
-        function upload(contentType, data, next) {
-            // Stream the transformed image to a different S3 bucket.
-            s3.putObject({
-                    Bucket: srcBucket,
-                    Key: dstKey,
-                    Body: data,
-                    ContentType: contentType
-                },
-                next);
-            }
-        ], function (err) {
-            if (err) {
-                console.error(
-                    'Unable to resize ' + srcBucket + '/' + srcKey + ' and upload to ' + srcBucket + '/' + dstKey + ' due to an error: ' + err
-                );
-            } else {
-                console.log(
-                    'Successfully resized ' + srcBucket + '/' + srcKey + ' and uploaded to ' + srcBucket + '/' + dstKey
-                );
-            }
-
-            context.done();
+        if (fileType === null) {
+            console.error("Invalid filetype found for key: " + srcKey);
+            return;
         }
-    );
+
+        fileType = fileType[0].substr(1);
+
+        if (ALLOWED_FILETYPES.indexOf(fileType) === -1) {
+            console.error("Filetype " + fileType + " not valid for thumbnail, exiting...");
+            return;
+        }
+
+        // Download the image from S3, transform, and upload to a different S3 bucket.
+         // Download the image from S3, transform, and upload to a different S3 bucket.
+        async.waterfall([
+            function download(next) {
+                // Download the image from S3 into a buffer.
+                s3.getObject({
+                        Bucket: srcBucket,
+                        Key: srcKey
+                    },
+                    next);
+                },
+            function transform(response, next) {
+                gm(response.Body).size(function(err, size) {
+                    // Infer the scaling factor to avoid stretching the image unnaturally.
+                    var scalingFactor = Math.min(
+                        MAX_WIDTH / size.width,
+                        MAX_HEIGHT / size.height
+                    );
+                    var width  = scalingFactor * size.width;
+                    var height = scalingFactor * size.height;
+
+                    // Transform the image buffer in memory.
+                    this.resize(width, height)
+                        .toBuffer(fileType, function(err, buffer) {
+                            if (err) {
+                                next(err);
+                            } else {
+                                next(null, response.ContentType, buffer);
+                            }
+                        });
+                });
+            },
+            function upload(contentType, data, next) {
+                // Stream the transformed image to a different S3 bucket.
+                s3.putObject({
+                        Bucket: srcBucket,
+                        Key: dstKey,
+                        Body: data,
+                        ContentType: contentType
+                    },
+                    next);
+                }
+            ], function (err) {
+                if (err) {
+                    console.error(
+                        'Unable to resize ' + srcBucket + '/' + srcKey + ' and upload to ' + srcBucket + '/' + dstKey + ' due to an error: ' + err
+                    );
+                } else {
+                    console.log(
+                        'Successfully resized ' + srcBucket + '/' + srcKey + ' and uploaded to ' + srcBucket + '/' + dstKey
+                    );
+                }
+
+                context.done();
+            });
+    } else {
+        context.done();
+    }
 };
